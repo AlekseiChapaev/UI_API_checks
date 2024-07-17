36a1ab8840d5f2eefdc1bc24aff8c66dd6d04a34
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-17 15:53:12 +0200

    remove Utils class. In project just abstract BaseTest and class with tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
-public class BaseTest extends Utils {
+public abstract class BaseTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 
     @BeforeMethod
     public void beforeMethod() {
+        driver = new ChromeDriver();
         getDriver().get("https://www.selenium.dev/selenium/web/");
         getDriver().manage().window().maximize();
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
@@ -14,6 +27,15 @@ public class BaseTest extends Utils {
 
     @AfterMethod
     public void afterMethod() {
-        getDriver().quit();
+        driver.quit();
     }
+
+    public WebDriverWait getWait() {
+        if(wait == null) {
+            wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+        return wait;
+    }
+
+
 }
