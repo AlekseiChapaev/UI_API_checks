10f97e336dbc26fbc37cebc4eff0b54c7d396140
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-17 13:22:19 +0200

    add labelTest

--- src/test/java/SeleniumFeaturesTest.java
+++ src/test/java/SeleniumFeaturesTest.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,4 +13,20 @@ public class SeleniumFeaturesTest extends BaseTest {
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "XHTML Might Be The Future");
     }
+
+    @Test
+    public void labelTest() {
+        final String text1 = "Label_1";
+
+        getDriver().findElement(By.linkText("ajaxy_page.html")).click();
+        getDriver().findElement(By.xpath("//input[@name = 'typer']")).sendKeys(text1);
+        getDriver().findElement(By.id("red")).click();
+        getDriver().findElement(By.xpath("//input[@name = 'submit']")).click();
+
+        WebElement label_1 = getWait().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class = 'label']")));
+        Assert.assertEquals(label_1.getText(), text1);
+        Assert.assertEquals(label_1.getAttribute("style"), "background-color: red;");
+    }
+
+
 }
--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,12 +1,25 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public abstract class Utils {
 
     private final WebDriver driver = new ChromeDriver(new ChromeOptions());
+    private WebDriverWait wait;
 
     public WebDriver getDriver() {
         return driver;
     }
+
+    public WebDriverWait getWait() {
+        if(wait == null) {
+            return new WebDriverWait(driver, Duration.ofSeconds(5));
+        }
+        return wait;
+    }
+
+
 }
