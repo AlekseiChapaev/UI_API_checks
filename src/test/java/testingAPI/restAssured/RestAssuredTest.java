88f5fa1f40c29b406d2a31fe70dc29ee0b3b4d0c
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-25 14:52:39 +0200

    Add dependency io.rest-assured for RestAssured. Add pokemonWithRestAssuredTest() with explanation

---
+++ src/test/java/testingAPI/restAssured/RestAssuredTest.java
@@ -0,0 +1,18 @@
+package testingAPI.restAssured;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+
+    @Test
+    public void pokemonWithRestAssuredTest() {
+        RestAssured.when()
+                .get("https://pokeapi.co/api/v2/pokemon") // here we send a request on necessary end point
+                .then()                                     //after this key word we start out checks
+                .statusCode(200)                         //here we check that status code = 200
+                .body("count", Matchers.equalTo(1302), //here we check that a response has a key count with value 1302
+                        "results.name", Matchers.hasItems("bulbasaur", "ivysaur")); // here we check wat in array results there are values bulbasaur and ivysaur
+    }
+}
