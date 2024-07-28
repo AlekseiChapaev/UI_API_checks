d4720201e71c04966662ea626d01832fa85531e9
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-28 14:38:37 +0200

    Add as(new TypeRef<>() {}) to convert response to object in positiveGetListPetByStatusAsObjectsTest() and .asString() to convert response to String in positiveGetListPetByStatusAsStringTest

--- src/test/java/testingAPI/restAssured/PetStoreTest.java
+++ src/test/java/testingAPI/restAssured/PetStoreTest.java
@@ -1,8 +1,13 @@
 package testingAPI.restAssured;
 
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,8 +19,10 @@ public class PetStoreTest {
 
     @Test
     public void positiveCheckPetNotExistTest() {
-        RestAssured.when()
-                .get(END_POINT.concat(PET_ID))
+        RestAssured.given()
+                .baseUri(END_POINT)
+                .when()
+                .get(PET_ID)
                 .then()
                 .statusCode(404)
                 .body("type", equalTo("error"))
@@ -25,9 +32,43 @@ public class PetStoreTest {
     @Test
     public void negativeCheckRequestWithoutPetIdTest() {
         RestAssured.when()
-                .get(END_POINT.concat(""))
+                .get(END_POINT.concat(""))  //https://petstore.swagger.io/v2/pet/""
                 .then()
                 .statusCode(405)
                 .statusLine(containsString("405 Method Not Allowed")); //here we check a notification into Response StatusCode row
+    }
+
+    @Test
+    public void positiveGetListPetByStatusAsStringTest() {
+        String listPetsAsString = RestAssured.given()
+                .baseUri(END_POINT)
+                .queryParam("status", "available")
+                .when()
+                .get("findByStatus/")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response()
+                .asString();
+    }
+
+    @Test
+    public void positiveGetListPetByStatusAsObjectsTest() {
+        List<Map<String, Object>> listPetsAsObjects = //мы ждем список, где каждый элемент Map это питомец, с ключами и значениями.
+                RestAssured.given()
+                        .baseUri(END_POINT)
+                        .queryParam("status", "available")
+                        .when()
+                        .get("findByStatus/")   // https://petstore.swagger.io/v2/pet/findByStatus?status=available
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response()
+                        .as(new TypeRef<>() {}); //указываем что тело ответа должно быть десериализовано в список Map
+        /*
+        Используем List<Map<String, Object>> вместо List<Map<String, String>>
+        потому что значения в JSON могут быть различных типов,
+        таких как строки, числа, логические значения, вложенные объекты и массивы
+         */
     }
 }
