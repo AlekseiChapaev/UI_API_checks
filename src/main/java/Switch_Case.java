684086e8e8bf2ccef1e0201d94a5fd00a31a3b97
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-30 11:41:47 +0200

    add switch-case

---
+++ src/main/java/Switch_Case.java
@@ -0,0 +1,18 @@
+public class Switch_Case {
+    public static String whatDay(int num) {
+        switch (num) {
+            case 1: return "Sunday";
+            case 2: return "Monday";
+            case 3: return "Tuesday";
+            case 4: return "Wednesday";
+            case 5: return "Thursday";
+            case 6: return "Friday";
+            case 7: return "Saturday";
+            default: return "Wrong, please enter a number between 1 and 7";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(whatDay(4));
+    }
+}
