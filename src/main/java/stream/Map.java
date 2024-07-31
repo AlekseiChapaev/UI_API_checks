f3021940aea255964d2ce3aa0bc84d4e008d7b88
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-31 12:18:11 +0200

    add to stream().map

--- src/main/java/stream/Map.java
+++ src/main/java/stream/Map.java
@@ -1,10 +1,21 @@
 package stream;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Map {
     public static int[] invert(int[] array) {
         return Arrays.stream(array).map(i -> (-1) * i).toArray();
+    }
+
+    public static String fakeBin(String numberString) {
+        return Arrays.stream(numberString.split("")).map(s -> {
+            if (Integer.parseInt(s) < 5) {
+                return "0";
+            } else {
+                return "1";
+            }
+        }).collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
