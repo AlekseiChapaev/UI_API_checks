a00f98233ef365667e6a505c1e14e824f614942c
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-07-31 15:09:35 +0200

    add Create stream through Stream.iterate() and IntStream.rangeClosed(num1, num2)

---
+++ src/main/java/stream/Create.java
@@ -0,0 +1,24 @@
+package stream;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Create {
+    public static String countingSheep(int num) {
+        return Stream.iterate(1, n -> n + 1)
+                .limit(num)
+                .map(n -> n + " sheep...")
+                .collect(Collectors.joining());
+    }
+
+    public static String countingSheep2(int num) {
+        return IntStream.rangeClosed(1, num)
+                .mapToObj(n -> n + " sheep...")
+                .collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countingSheep2(4));
+    }
+}
