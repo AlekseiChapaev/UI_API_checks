4d17471c98000957ad730eab16f5b38e8f62df2a
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-08-02 10:31:03 +0200

    add example to MapToInt stream

--- src/main/java/stream/MapToInt.java
+++ src/main/java/stream/MapToInt.java
@@ -1,5 +1,6 @@
 package stream;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MapToInt {
@@ -16,8 +17,20 @@ public class MapToInt {
         //return mixed.stream().mapToInt(o -> Integer.parseInt(o.toString())).sum();
     }
 
+    public static int points(String[] games) {
+        return Arrays.stream(games).mapToInt(s -> {
+            if (Integer.parseInt(s.substring(0, 1)) > Integer.parseInt(s.substring(2))) {
+                return 3;
+            } else if (Integer.parseInt(s.substring(0, 1)) == Integer.parseInt(s.substring(2))) {
+                return 1;
+            } else return 0;
+        }).sum();
+    }
+
     public static void main(String[] args) {
-        System.out.println(sum(List.of("3", "5", 2, 5)));
+        System.out.println(sum(List.of("3", "5", 2, 10)));
+        System.out.println(points(new String[]
+                {"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"}));
     }
 
 }
