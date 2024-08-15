543173291e8f50cb8b52021028fc566a90f88228
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-08-15 15:55:01 +0200

    add post method and example of text block for request body in LinkedIN api_learning

--- src/test/java/testingAPI/restAssured/LinkedINTrainingTest.java
+++ src/test/java/testingAPI/restAssured/LinkedINTrainingTest.java
@@ -25,4 +25,20 @@ public class LinkedINTrainingTest {
 
         response.log().body();
   }
+
+  @Test
+   public void createProduct() {
+        String endpoint = "http://localhost:8888/api_testing/product/create.php";
+        String body = """
+                {
+                "name": "Water battle",
+                "description": "Blue water bottle. Holds 64 ounces",
+                "price": 12,
+                "category_id": 3
+                }
+                """;
+        var response = given().body(body).when().post(endpoint).then();
+
+        response.log().body();
+  }
 }
