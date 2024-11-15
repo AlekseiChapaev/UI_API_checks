57a2db2e93f0f4f343774a2c4a8941182bcd572c
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-11-15 20:13:49 +0100

    add testShiftKeyThroughActions to show how send keys through Actions

--- src/test/java/testingUI/UsingKeysTest.java
+++ src/test/java/testingUI/UsingKeysTest.java
@@ -1,6 +1,7 @@
 package testingUI;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import testingUI.pages.base.BaseTest;
@@ -9,7 +10,7 @@ public class UsingKeysTest extends BaseTest {
 
 
     @Test
-    public void testRange() {
+    public void testArrowKeys() {
         getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
         WebElement slider = getDriver().findElement(By.name("my-range"));
         slider.sendKeys(Keys.RIGHT, Keys.RIGHT);
@@ -17,4 +18,21 @@ public class UsingKeysTest extends BaseTest {
 
         Assert.assertEquals(slider.getAttribute("value"), "8");
     }
+
+    @Test
+    public void testShiftKeyThroughActions() {
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textArea = getDriver().findElement(By.name("my-textarea"));
+
+        new Actions(getDriver())
+                .click(textArea)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("qwerty")
+                .keyUp(Keys.SHIFT)
+                .perform();
+
+        System.out.println(textArea.getAttribute("value"));
+        Assert.assertEquals(textArea.getAttribute("value"), "QWERTY");
+    }
+
 }
