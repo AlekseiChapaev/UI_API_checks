f43abfce01eb029cbe12c4a2ed548ab3b6a76b28
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-11-15 20:48:05 +0100

    add testDragAndDropWebElementToAnotherWebElement

--- src/test/java/testingUI/Actions_DragAndDropTest.java
+++ src/test/java/testingUI/Actions_DragAndDropTest.java
@@ -35,7 +35,7 @@ public class Actions_DragAndDropTest extends BaseTest {
     }
 
     @Test
-    public void testDragAndDropBy() {
+    public void testDragAndDropByPuttingCoordinate() {
         getDriver().get("https://www.selenium.dev/selenium/web/dragAndDropTest.html");
         WebElement test1 = getDriver().findElement(By.id("test1"));
 
@@ -56,5 +56,28 @@ public class Actions_DragAndDropTest extends BaseTest {
 
         Assert.assertEquals(newLocation.getX(), oldLocation.getX() + shiftX);
         Assert.assertEquals(newLocation.getY(), oldLocation.getY() + shiftY);
+    }
+
+    @Test
+    public void testDragAndDropWebElementToAnotherWebElement() {
+        getDriver().get("https://www.selenium.dev/selenium/web/dragAndDropTest.html");
+        WebElement test1 = getDriver().findElement(By.id("test1"));
+        WebElement test4 = getDriver().findElement(By.id("test4"));
+
+        Point test1OldLocation = test1.getLocation();
+        System.out.println("element test1 oldLocation: " + test1OldLocation);
+        Point test4Location = test4.getLocation();
+        System.out.println("element test4 location: " + test4Location);
+        new Actions(getDriver())
+                .dragAndDrop(test1, test4)
+                .release()
+                .perform();
+
+        Point test1NewLocation = test1.getLocation();
+        System.out.println("test1NewLocation: " + test1NewLocation);
+
+        Assert.assertEquals(test4Location, test1NewLocation);
+        Assert.assertEquals(test1NewLocation.getX(), test4Location.getX());
+        Assert.assertEquals(test1NewLocation.getY(), test4Location.getY());
     }
 }
