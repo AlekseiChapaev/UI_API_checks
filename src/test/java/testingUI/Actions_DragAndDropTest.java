e52f2a7f6588b4d5ecf91192edddac08e5297bcc
Aleksei Chapaev <alekseiachapaev@gmail.com>
2024-11-15 20:40:13 +0100

    add dragAndDropBy

--- src/test/java/testingUI/Actions_DragAndDropTest.java
+++ src/test/java/testingUI/Actions_DragAndDropTest.java
@@ -11,15 +11,15 @@ import testingUI.pages.base.BaseTest;
 public class Actions_DragAndDropTest extends BaseTest {
 
     @Test
-    public void testDragAndDrop() {
+    public void testDragAndDropThroughClickAndHoldAndMoveByOffset() {
         getDriver().get("https://www.selenium.dev/selenium/web/dragAndDropTest.html");
         WebElement test1 = getDriver().findElement(By.id("test1"));
 
         Point oldLocation = test1.getLocation();
         System.out.println("oldLocation: " + oldLocation);
 
-        final int shiftToX = 100;
-        final int shiftToY = 50;
+        final int shiftX = 100;
+        final int shiftY = 50;
 
         new Actions(getDriver())
                 .clickAndHold(test1)
@@ -30,8 +30,31 @@ public class Actions_DragAndDropTest extends BaseTest {
         Point newLocation = test1.getLocation();
         System.out.println("newLocation: " + newLocation);
 
-        Assert.assertEquals(newLocation.getX(), oldLocation.getX() + shiftToX );
-        Assert.assertEquals(newLocation.getY(), oldLocation.getY() + shiftToY);
+        Assert.assertEquals(newLocation.getX(), oldLocation.getX() + shiftX);
+        Assert.assertEquals(newLocation.getY(), oldLocation.getY() + shiftY);
+    }
 
+    @Test
+    public void testDragAndDropBy() {
+        getDriver().get("https://www.selenium.dev/selenium/web/dragAndDropTest.html");
+        WebElement test1 = getDriver().findElement(By.id("test1"));
+
+        Point oldLocation = test1.getLocation();
+        System.out.println("oldLocation: " + oldLocation);
+
+        final int shiftX = 200;
+        final int shiftY = 200;
+
+        new Actions(getDriver())
+                .dragAndDropBy(test1, shiftX, shiftY)
+                .moveByOffset(100, 50)
+                .release()
+                .perform();
+
+        Point newLocation = test1.getLocation();
+        System.out.println("newLocation: " + newLocation);
+
+        Assert.assertEquals(newLocation.getX(), oldLocation.getX() + shiftX);
+        Assert.assertEquals(newLocation.getY(), oldLocation.getY() + shiftY);
     }
 }
